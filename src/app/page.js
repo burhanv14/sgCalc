9c4345b056f0e2d5c96cafac8be4777ce9c5a43b
Burhanuddin Vora <burhanuddin.vora@gmail.com>
2024-06-19 17:25:27 +0530

    added animation

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,8 @@ import { Jacquard_24 } from 'next/font/google'
 import { Jacquard_12_Charted } from 'next/font/google'
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
+import Spline from '@splinetool/react-spline'
+
 
 
 const jackquard = Jacquard_24({
@@ -116,7 +118,9 @@ export default function Page() {
 
   return (
     <div class="flex flex-col w-full">
-      <div class="flex justify-between w-full sm:max-md:flex-wrap md:h-28 bg-zinc-800">
+      
+      
+      <div class="flex justify-between w-full sm:max-md:flex-wrap md:h-28 bg-black">
         <h1 class="flex-row w-1/2 text-white sm:max-md:text-sm text-5xl text-left md:p-6 p-3" >
           <span className={jackquard.className}>S.G.P.A. Abacus</span>
         </h1>
@@ -130,11 +134,14 @@ export default function Page() {
           />
         </div>
      </div>
-      <div class="min-h-screen h-auto bg-stone-800 pt-1 px-4 md:p-4 w-full">
-       <form class="md:py-44 py-32">
+
+
+      <div class="container min-h-screen h-auto bg-black w-full flex">
+      <div class=" pt-1 px-4 md:p-4 h-auto w-auto flex-row">
+       <form class="md:py-28 py-32">
         {inputs.map((input, i) => (
-        <div key={i} class="md:pl-64 pl-4">
-          <h1 class="text-4xl text-white text-opacity-100 py-2"><span className={jackquard2.className}>Subject : {i+1}</span></h1>
+        <div key={i} class="md:pl-20 pl-4">
+          <h1 class="text-4xl text-white text-opacity-100 py-2"><span className={jackquard.className}>Subject : {i+1}</span></h1>
           <div class="flex flex-col md:flex-row gap-4">
           <div class="flex flex-row gap-4">
           <input
@@ -162,39 +169,43 @@ export default function Page() {
           <option value="F">F</option>
           </select>
          </div>
-          <div class="flex md:flex-row md:pt-2 md:pl-48 gap-3">
+          <div class="flex md:flex-row md:pt-2 md:pl-20 gap-3">
           {inputs.length - 1 === i && (
             <button type="button" class="font-serif text-white bg-gray-800 hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 md:py-1 dark:focus:ring-gray-700 dark:border-gray-700" onClick={() => handleAddClick(i)}>Add</button>
           )}
           {inputs.length !== 1 && (
-            <button type="button" class="font-serif text-white bg-gray-800 hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5  me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 md:py-1 dark:focus:ring-gray-700 dark:border-gray-700" onClick={() => handleRemoveClick(i)}>Remove</button>
+            <button type="button" class="font-serif text-white bg-red-900  hover:bg-red-700 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5  me-2 mb-2  md:py-1 " onClick={() => handleRemoveClick(i)}>Remove</button>
           )}
           </div>
         </div>
+       </div>
+       ))
+       }
+        <div class="pt-8 pl-4 md:pt-16 md:pl-20 flex gap-6 md:gap-10">
+        <button onClick={event => handleSubmit(event,inputs)}>
+        <a href="#_" class="relative px-5 py-2 font-medium text-white group">
+        <span class="absolute inset-0 w-full h-full transition-all duration-300 ease-out sm:transform translate-x-0 -skew-x-12 bg-black group-hover:bg-purple-700 group-hover:skew-x-12"></span>
+        <span class="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform skew-x-12 bg-black group-hover:bg-purple-500 group-hover:-skew-x-12"></span>
+        <span class="absolute bottom-0 left-0 hidden w-10 h-20 transition-all duration-100 ease-out transform -translate-x-8 translate-y-10 bg-purple-600 -rotate-12"></span>
+        <span class="absolute bottom-0 right-0 hidden w-10 h-20 transition-all duration-100 ease-out transform translate-x-10 translate-y-8 bg-purple-400 -rotate-12"></span>
+        <span class="relative font-serif">Submit</span>
+        </a>
+        </button>
+      <a href="#_" class="relative px-5 py-1.5 font-medium text-white group" onClick={() => handleResetClick()}>
+      <span class="absolute inset-0 w-full h-full transition-all duration-300 ease-out sm:transform translate-x-0 -skew-x-12 bg-black group-hover:bg-purple-700 group-hover:skew-x-12"></span>
+      <span class="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform skew-x-12 bg-black group-hover:bg-purple-500 group-hover:-skew-x-12"></span>
+      <span class="absolute bottom-0 left-0 hidden w-10 h-20 transition-all duration-100 ease-out transform -translate-x-8 translate-y-10 bg-purple-600 -rotate-12"></span>
+      <span class="absolute bottom-0 right-0 hidden w-10 h-20 transition-all duration-100 ease-out transform translate-x-10 translate-y-8 bg-purple-400 -rotate-12"></span>
+      <span class="relative font-serif" onClick={() => handleResetClick()}>Reset</span>
+      </a>
       </div>
-    ))
-    }
-    <div class="pt-8 pl-4 md:pt-16 md:pl-64 flex gap-6 md:gap-10">
-    <button onClick={event => handleSubmit(event,inputs)}>
-    <a href="#_" class="relative px-5 py-2 font-medium text-white group">
-    <span class="absolute inset-0 w-full h-full transition-all duration-300 ease-out sm:transform translate-x-0 -skew-x-12 bg-black group-hover:bg-purple-700 group-hover:skew-x-12"></span>
-    <span class="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform skew-x-12 bg-black group-hover:bg-purple-500 group-hover:-skew-x-12"></span>
-    <span class="absolute bottom-0 left-0 hidden w-10 h-20 transition-all duration-100 ease-out transform -translate-x-8 translate-y-10 bg-purple-600 -rotate-12"></span>
-    <span class="absolute bottom-0 right-0 hidden w-10 h-20 transition-all duration-100 ease-out transform translate-x-10 translate-y-8 bg-purple-400 -rotate-12"></span>
-    <span class="relative font-serif">Submit</span>
-    </a>
-    </button>
-    <a href="#_" class="relative px-5 py-1.5 font-medium text-white group" onClick={() => handleResetClick()}>
-    <span class="absolute inset-0 w-full h-full transition-all duration-300 ease-out sm:transform translate-x-0 -skew-x-12 bg-black group-hover:bg-purple-700 group-hover:skew-x-12"></span>
-    <span class="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform skew-x-12 bg-black group-hover:bg-purple-500 group-hover:-skew-x-12"></span>
-    <span class="absolute bottom-0 left-0 hidden w-10 h-20 transition-all duration-100 ease-out transform -translate-x-8 translate-y-10 bg-purple-600 -rotate-12"></span>
-    <span class="absolute bottom-0 right-0 hidden w-10 h-20 transition-all duration-100 ease-out transform translate-x-10 translate-y-8 bg-purple-400 -rotate-12"></span>
-    <span class="relative font-serif" onClick={() => handleResetClick()}>Reset</span>
-    </a>
+        {totalCredits!=0 && showRes && <h1 class="text-4xl md:text-5xl text-white text-opacity-100 py-10 pl-3 md:p-40 md:py-16"><span className={jackquard2.className}>Current SGPA :{(numCredits/totalCredits) * 10}</span></h1>}
+      </form>
     </div>
-    {totalCredits!=0 && showRes && <h1 class="text-4xl md:text-5xl text-white text-opacity-100 py-10 pl-3 md:p-128 md:py-16"><span className={jackquard2.className}>Current SGPA : {(numCredits/totalCredits) * 10}</span></h1>}
-    </form>
-   </div>
+    <div className="hidden md:block md:pt-0 md:absolute md:right-0 md:top-7 md:h-full md:w-1/2">
+            <Spline scene="https://prod.spline.design/53UfXp-hqeAip-6w/scene.splinecode"/>
+    </div>
+    </div>
   </div>
   )
 }
